7974c5baa779b072d06ea50313f3b9a31eeec0b6
zuudevs <zuudevs@gmail.com>
2025-12-23 22:26:44 +0700

    update, added interface

--- P1/web/script.js
+++
@@ -1,6 +0,0 @@
-// File ini bisa digunakan untuk menambahkan interaktivitas di masa depan.
-// Misalnya, tombol untuk menyalin kode.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Web Pertemuan 1 siap!');
-});
